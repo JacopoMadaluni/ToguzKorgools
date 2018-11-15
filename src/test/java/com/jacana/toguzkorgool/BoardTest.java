5c02fd5f3b68035720d17657759b3b9c5015485e
JacopoMadaluni <k1763985@kcl.ac.uk>
2018-11-15 17:23:24 +0000

    Add tuz rule + tuz rule tests

--- src/test/java/com/jacana/toguzkorgool/BoardTest.java
+++ src/test/java/com/jacana/toguzkorgool/BoardTest.java
@@ -1,10 +1,24 @@
 package com.jacana.toguzkorgool;
+import javafx.application.Platform;
 import org.junit.Test;
+
+import javax.management.DescriptorKey;
 
 public class BoardTest {
 
     @Test
     public void sampleTest(){
         // ...
+    }
+
+    @Test
+    public void testChangePlayer(){
+        Board board = new Board();
+        Player player1 = board.getCurrentPlayer();
+        Player bot = board.getNextPlayer();
+        board.changePlayer();
+        assert(board.getCurrentPlayer().equals(bot));
+        board.changePlayer();
+        assert(board.getCurrentPlayer().equals(player1));
     }
 }
