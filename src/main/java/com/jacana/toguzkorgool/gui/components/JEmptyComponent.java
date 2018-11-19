0cec72f176b668d5db8d8b96e50b889d0478fe95
David Simon Tetruashvili <david.tetruashvili@kcl.ac.uk>
2018-11-19 20:47:28 +0000

    Link GUI holes
    Linked GUI holes to back-end counterparts.

--- src/main/java/com/jacana/toguzkorgool/gui/components/JEmptyComponent.java
+++ src/main/java/com/jacana/toguzkorgool/gui/components/JEmptyComponent.java
@@ -2,5 +2,9 @@ package com.jacana.toguzkorgool.gui.components;
 
 import javax.swing.JComponent;
 
+/**
+ * Empty component which is meant to replace a korgool in the case where there shouldn't be one.
+ * Renders nothing to the screen, but maintains the sie constraint.
+ */
 public class JEmptyComponent extends JComponent {
 }
