05de6bd5d7a1637e4e9e77aa0e61b5a602fc359d
Tetruashvili, David (k1764158) <david.tetruashvili@kcl.ac.uk>
2018-11-26 00:35:13 +0000

    Make JKorgool conform to code review

--- src/main/java/com/jacana/toguzkorgool/gui/components/JKorgool.java
+++ src/main/java/com/jacana/toguzkorgool/gui/components/JKorgool.java
@@ -5,8 +5,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 /**
- * @author Rehman, Faris (k1764099), Bass, Alexander (k1763794).
- * <p>
  * JKorgool is a graphical representation of a single Korgool in the game of
  * Toguz Korgool.
  */
