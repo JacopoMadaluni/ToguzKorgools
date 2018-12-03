6348787b5e3f948537bec06d584b00e4deb5663e
K1764099 <faris.rehman@kcl.ac.uk>
2018-12-03 21:09:51 +0000

    Add game loading
    Add game loading functionality

--- src/main/java/com/jacana/toguzkorgool/Board.java
+++ src/main/java/com/jacana/toguzkorgool/Board.java
@@ -95,8 +95,9 @@ public class Board {
         return opponent.getKorgoolsInHole(index);
     }
 
+    // TODO: Confirm if this is calling the correct method.
     public int getOpponentTuz() {
-        return getCurrentOpponent().getTuzIndex();
+        return getCurrentOpponent().getOpponentTuzIndex();
     }
 
     /**
