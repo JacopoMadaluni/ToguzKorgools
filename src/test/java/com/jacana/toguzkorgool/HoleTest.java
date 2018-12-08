3a24fc19815d82843644ed98a937e365d0a140fe
K1764099 <faris.rehman@kcl.ac.uk>
2018-12-08 16:42:18 +0000

    Implement more Hole tests

--- src/test/java/com/jacana/toguzkorgool/HoleTest.java
+++ src/test/java/com/jacana/toguzkorgool/HoleTest.java
@@ -13,6 +13,21 @@ public class HoleTest {
     }
 
     @Test
+    public void testAdd() {
+        Hole hole = new Hole();
+        hole.add(1);
+        assertEquals(1, hole.getKorgools());
+    }
+
+    @Test
+    public void testClear() {
+        Hole hole = new Hole();
+        hole.add(1);
+        hole.clear();
+        assertEquals(0, hole.getKorgools());
+    }
+
+    @Test
     public void testTuz() {
         Hole hole = new Hole();
         hole.setTuz(true);
