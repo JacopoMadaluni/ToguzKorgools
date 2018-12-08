7de9581ecf16f79faaed940366271e07d20ca575
K1764099 <faris.rehman@kcl.ac.uk>
2018-12-08 16:22:35 +0000

    Implement hole tests
    Implement basic Hole tests.

--- src/test/java/com/jacana/toguzkorgool/HoleTest.java
+++ src/test/java/com/jacana/toguzkorgool/HoleTest.java
@@ -1,10 +1,30 @@
 package com.jacana.toguzkorgool;
+
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class HoleTest {
 
     @Test
-    public void sampleTest(){
-        // ...
+    public void testInitialState() {
+        Hole hole = new Hole();
+        assertEquals(9, hole.getKorgools());
     }
+
+    @Test
+    public void testTuz() {
+        Hole hole = new Hole();
+        hole.setTuz(true);
+        assertTrue(hole.isTuz());
+    }
+
+    @Test
+    public void testTuzToggling() {
+        Hole hole = new Hole();
+        hole.setTuz(true);
+        hole.setTuz(false);
+        assertFalse(hole.isTuz());
+    }
+
 }
