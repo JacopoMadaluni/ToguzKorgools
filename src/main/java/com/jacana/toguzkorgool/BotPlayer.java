2143fbaa618985987dc081742b61542b3f5431c0
K1763985 <jacopo.madaluni@kcl.ac.uk>
2018-12-13 14:07:02 +0000

    Add Players comments.

--- src/main/java/com/jacana/toguzkorgool/BotPlayer.java
+++ src/main/java/com/jacana/toguzkorgool/BotPlayer.java
@@ -2,14 +2,27 @@ package com.jacana.toguzkorgool;
 
 import java.util.Random;
 
+/**
+ * This class represents a non human player.
+ * It can be extended with a more advanced logic system behind the
+ * bot moves.
+ */
 public class BotPlayer extends Player {
 
     private Random random = new Random();
 
+    /**
+     * Creates a new bot.
+     * @param board The board it's playing in.
+     * @param id The id of the bot player.
+     */
     public BotPlayer(Board board, int id) {
         super(board, id);
     }
 
+    /**
+     * The bot makes its move when this method is called.
+     */
     public void act() {
         int randomIndex = random.nextInt(holes.length);
         super.makeMove(randomIndex);
--- src/main/java/com/jacana/toguzkorgool/HumanPlayer.java
+++ src/main/java/com/jacana/toguzkorgool/HumanPlayer.java
@@ -1,7 +1,17 @@
 package com.jacana.toguzkorgool;
 
+/**
+ * This class represents a Human Player.
+ * It can be extended with more advanced functionalities in the moment
+ * that two human players play against each other.
+ */
 public class HumanPlayer extends Player {
 
+    /**
+     * Create a new Human Player
+     * @param board The board he's playing on.
+     * @param id The id of the player.
+     */
     public HumanPlayer(Board board, int id){
         super(board, id);
     }
